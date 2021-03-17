bfd79e9e755cdab2c54ffc06d0839b18a742f7d3
pedrowd <75539722+pedrowd@users.noreply.github.com>
2021-03-16 22:39:36 -0300

    unable
    ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooxoooooooooooooox
    oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo  oooooooooooo

--- unablecell.py
+++ unablecell.py
@@ -1,3 +1,16 @@
-import cell
-def unable(x):
-  
+inport random
+def inposo(x):
+  def poso():
+    lis = ["o         ", " o        ", "  o       ", "   o      ", "     o    ", "     o    ", "      o   ", "       o  ", "        o ", "         o"]
+    ranpos = random.choice(lis)
+    print(ranpos)
+  pos(x)
+def unablecellmake(x, y, z, e, o):
+  cell = x
+  pos1 = y
+  pos2 = z
+  type = e
+  name = o
+  i = inposo(cell)
+  print(i, name, type)
+  print(i, name, type)
